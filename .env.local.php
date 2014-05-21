eb3f119828533f4aed6c6bcbf2423305b9fc17ff
Stygian <stygian.warlock.v2@gmail.com>
2014-05-21 09:40:36 -0500

    Fixing git ignore and env file

--- .env.local.php
+++ .env.local.php
@@ -1,17 +1,3 @@
 <?php
 
-return [
-    // Used in app/configs/remote.php
-    'remote_host'   => 'localhost:1979',
-    'remote_user'   => 'stygian',
-    'remote_pass'   => 'stygicide',
-    'remote_key'    => '',
-    'remote_phrase' => '',
-    'remote_home'   => '/home/stygian/dev/base',
-
-    // Used in app/configs/database.php
-    'database_host' => 'DB_HOST',
-    'database_name' => 'DB_NAME',
-    'database_user' => 'DB_USER',
-    'database_pass' => 'DB_PASS'
-];
+return [];
