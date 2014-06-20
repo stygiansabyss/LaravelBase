6581ee208f2874e6bc2d5d9c0fed25925837a161
Stygian <stygian.warlock.v2@gmail.com>
2014-06-20 15:18:05 -0500

    PACK-10: Minor update to add injection to the base controller

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -1,4 +1,19 @@
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Routing\Redirector;
+
 class BaseController extends MenuController {
+
+    protected $input;
+
+    protected $redirect;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->input    = App::make('Illuminate\Http\Request');
+        $this->redirect = App::make('Illuminate\Routing\Redirector');
+    }
 }
