324f89d9e0be3a191f4eeaaa20f79ff08aef897a
Stygian <stygian.warlock.v2@gmail.com>
2014-06-23 01:57:20 -0500

    PACK-10: Updating version

--- app/config/packages.php
+++ app/config/packages.php
@@ -3,7 +3,7 @@
 return [
     'nukacode' => [
         'laravel-base' => [
-            'version' => '1.0.4',
+            'version' => '1.0.5',
             'color'   => 'inverse',
             'icon'    => 'fa-code'
         ],
